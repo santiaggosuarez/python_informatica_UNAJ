00a629838846d4bf375ea8a1ae862bf7f791ef5b
Santiago Suárez <santiaggosuarez@gmail.com>
2022-05-07 16:00:00 -0300

    Create P3_ej5_Santiago_Suarez.py
    Ejercicio 5: Definir una función denominada cuantos_dias que reciba el número de mes como parámetro y retorne la cantidad de días que posee. Ejemplo: cuantos_dias(1), debería retornar 31.

---
+++ P3_ej5_Santiago_Suarez.py
@@ -0,0 +1,13 @@
+#lista de meses
+
+meses = [["ERROR","ingrese un numero del 1 al 12"],["enero",31],["febrero",28],["marzo",31],["abril",30],["mayo",31],["junio",30],["julio",31],["agosto",31],["septiembre",30],["octubre",31],["noviembre",30],["diciembre",31]]
+
+#funcion de cuantos dias tiene el mes (x)
+
+def cuantos_dias(x):
+  return meses[x][1]
+
+#damos valor a la variable x
+
+x = int(input("ingrese el número del mes: "))
+print(cuantos_dias(x))
