86bbf39568abd8480aedf60f658c14f6be781c80
Santiago Suárez <santiaggosuarez@gmail.com>
2022-05-30 17:02:25 -0300

    Create P4_ej2_Santiago_Suarez.py

---
+++ P4_ej2_Santiago_Suarez.py
@@ -0,0 +1,15 @@
+#Ejercicio 2: Escribir un programa que lea dos números “n” y “m” y determine si n es divisible por m, mostrando el siguiente mensaje por pantalla:
+# “El número n es/no es divisible por m”. Reemplazar n y m por los números correspondientes.
+
+def dos_numeros(n,m):
+  if (n%m) == 0:
+    resultado = "el número " + str(n) + " es divisible por " + str(m)
+  
+  else:
+    resultado =  "el número " + str(n) + " NO es divisible por " + str(m)
+
+  return resultado
+
+n = int(input("ingrese el dividendo: "))
+m = int(input("ingrese el divisor: "))
+print(dos_numeros(n,m))
