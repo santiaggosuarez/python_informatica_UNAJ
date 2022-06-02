8881ea136ecb8a890340aa906135908f922fa4eb
Santiago Suárez <santiaggosuarez@gmail.com>
2022-06-02 16:36:24 -0300

    Update P4_ej14_Santiago_Suarez.py

--- P4_ej14_Santiago_Suarez.py
+++ P4_ej14_Santiago_Suarez.py
@@ -1,20 +1,17 @@
 #Ejercicio 14: Calcular el número de pulsaciones que una persona debe tener por cada 10 segundos de ejercicio aeróbico;
 #la fórmula que se aplica cuando la persona no está entrenada: (220-edad)/10; si la persona está entrenada: (210-edad)/10.
 
-def entrenada(respuesta):
-  return respuesta == si or "si" or "SI" or "sí" or "Sí"
+entrenado = input("usted está entrenado/a?: ")
+edad = int(input("ingrese su edad: "))
 
-def numero_de_pulsaciones(edad,entrenada):
+def numero_de_pulsaciones(edad,entrenado):
 
-  if entrenada:
+  if entrenado == "si":
     resultado = (210 - edad)/10
   
-  else:
+  elif entrenado == "no":
     resultado = (220 - edad)/10
   
   return resultado
-
-respuesta = input("usted está entrenado/a?: ")
-edad = int(input("ingrese su edad: "))
-
-print("cada 10 segundos debe tener ",numero_de_pulsaciones(edad,entrenada)," pulsaciones.")
+  
+print("cada 10 segundos debe tener ",numero_de_pulsaciones(edad,entrenado)," pulsaciones.")
