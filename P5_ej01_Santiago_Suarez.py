4a5366e11fc2c85e1b4581a57e3ca642d937bb49
Santiago Suárez <santiaggosuarez@gmail.com>
2022-06-06 19:03:03 -0300

    Update P5_ej01_Santiago_Suarez.py

--- P5_ej01_Santiago_Suarez.py
+++ P5_ej01_Santiago_Suarez.py
@@ -1,8 +1,11 @@
 #Ejercicio 1: Escribir un programa que muestre la tabla de multiplicar de un número introducido por teclado por el usuario.
 
-num = int(input("ingrese un número para saber su tabla de multiplicar: "))
-mult = 1
+def tabla_de_multiplicar(num):
+  mult = 1
+  
+  while mult < 11:
+    print(num, " x ", mult, " = ", num*mult)
+    mult +=1
 
-while mult < 11:
-  print(num, " x ", mult, " = ", num*mult)
-  mult +=1
+n = int(input("ingrese un número para saber su tabla de multiplicar: "))
+tabla_de_multiplicar(n)
